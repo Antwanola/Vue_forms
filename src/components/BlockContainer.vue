cf1741b9ab2c016a336ac1627f3287e705f79feb
antwanola <antwanola29@gmail.com>
2023-11-12 20:45:44 +0100

    Refreshing emit skills

---
+++ src/components/BlockContainer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    
+    data() {
+      return {
+        showBlock: false,
+        timer: 0,
+        reationTimer: 0
+      }
+    },
+    mounted(){
+      console.log("Block Mounted");
+      setTimeout(() => {
+          this.showBlock = true;
+          this.startTimer();
+        }, this.delay);
+    },
+    updated() {
+      console.log("Block updated")
+    },
+
+    methods: {
+      startTimer() {
+        this.timer = setInterval(() => {
+          this.reationTimer += 10;
+        }, 10);
+      },
+      stopTimer() {
+        clearInterval(this.timer);
+        this.$emit('reactTimer', this.reationTimer)
+      }
+    },
+  
+
+
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+  <h1> Home page</h1>
+ <p>Score: {{ score}}</p>
+ <button class="first" @click="start" :disabled="isPlaying">Change Name</button>
+ <BlockContainer v-if="isPlaying" :delay="delay" @reactTimer="endGame"/>
   </div>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import BlockContainer from '../components/BlockContainer.vue'
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  components: {BlockContainer},
+  data() {
+    return {
+      isPlaying: false,
+      delay: 0,
+      score: 0
+    }
   },
-});
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = !this.isPlaying;
+      console.log(this.delay);
+    },
+    endGame(eventGetter: number) {
+      this.score = eventGetter
+    }
+  }
+  })
 </script>
+ <style>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.first {
+  padding: 2%
+}
+</style>
